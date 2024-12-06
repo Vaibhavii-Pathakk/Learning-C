2b2a2fc4e244e460e2b608056ca7f22edb8f9693
Vaibhavi Pathak <pathakvaibhavi34@gmail.com>
2024-12-06 13:02:52 +0530

    change words

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This Repository  will have all my programs of C language - 1st year.
 <br>
 Author: VAIBHAVI PATHAK.
+Date: 6th - DEC - 2024.
+
---
+++ index.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a = 24;
+    int b = 67;
+    int sum = a+b;
+    printf("The summation is:%d\n",sum);
+    return 0;
+}
